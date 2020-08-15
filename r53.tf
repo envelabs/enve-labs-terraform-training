b0a6158123c3d00ac4f879f2bc4dffe1374f589a
buglione <mariano@enve.io>
2020-08-15 14:21:56 -0300

    enable elb config

--- r53.tf
+++ r53.tf
@@ -1,7 +1,9 @@
 resource "aws_route53_record" "enve-webapp-srvr1" {
   zone_id = "Z04164709DVPTWJDONB9"
   name    = "enve-webapp-srvr1.envelabs.com"
-  type    = "A"
+  #type    = "A"
+  type    = "CNAME"
   ttl     = "300"
-  records = [aws_instance.enve-webapp-srvr1.public_ip]
+  # records = [aws_instance.enve-webapp-srvr1.public_ip]
+  records = [aws_elb.enve-webapp-elb.dns_name]
 }
