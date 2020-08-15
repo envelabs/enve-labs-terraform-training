1accf0c3fea1a04f5040c15f840008ed51ae5506
buglione <mariano@enve.io>
2020-08-15 10:40:30 -0300

    added support for envelabs.com domain

--- r53.tf
+++ r53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "enve-webapp-srvr1" {
-  zone_id = "Z97VZFZN8B5ZN"
-  name    = "enve-webapp-srvr1.justicialista.com"
+  zone_id = "Z04164709DVPTWJDONB9"
+  name    = "enve-webapp-srvr1.envelabs.com"
   type    = "A"
   ttl     = "300"
   records = [aws_instance.enve-webapp-srvr1.public_ip]
