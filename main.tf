249a96c036acf2bb8e480203067b11260cd31eb5
buglione <mariano@enve.io>
2020-08-15 10:39:20 -0300

    added data variables reference

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region  = "us-east-1"
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_availability_zones" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
