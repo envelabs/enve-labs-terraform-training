b392c8095957a2d6a43d1d86186cff141adaf0ae
buglione <mariano@enve.io>
2020-08-15 14:21:12 -0300

    enable asg

--- asg.tf
+++ asg.tf
@@ -1,9 +1,10 @@
 resource "aws_autoscaling_group" "enve-webapp-asg" {
   launch_configuration = aws_launch_configuration.enve-webapp-lc.name
-  availability_zones = ["us-east-1a"]
+  availability_zones = ["us-east-1a","us-east-1c"]
+  # todo
   # availability_zones = [data.aws_availability_zones.all.names]
-  # load_balancers= [aws_elb.enve-webapp-elb.id]
-  # health_check_type="ELB"
+  load_balancers= [aws_elb.enve-webapp-elb.id]
+  health_check_type="ELB"
 
   min_size = 2
   max_size = 5
