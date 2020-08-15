588122d8c7e3618bb3c5b8aeb9a955065f207334
buglione <mariano@enve.io>
2020-08-15 14:20:24 -0300

    fixed issue with listener config

--- elb.tf
+++ elb.tf
@@ -1,7 +1,19 @@
 resource "aws_elb" "enve-webapp-elb" {
   name               = "enve-webapp-elb"
-  subnets            = [data.aws_availability_zones.all.names]
+  # todo
+  # subnets            = [data.aws_subnet_ids.all.ids]
+  # subnets            = ["subnet-06f02c71", "subnet-3707191f"]
+  # todo
+  # availability_zones = [data.aws_availability_zones.all.names]
+  availability_zones = ["us-east-1a","us-east-1c"]
   security_groups    = [aws_security_group.enve-webapp-elb-sg.id]
+
+  listener {
+    instance_port      = var.http_port
+    instance_protocol  = "http"
+    lb_port            = 8080
+    lb_protocol        = "http"
+  }
 
   health_check {
     healthy_threshold   = 2
@@ -11,11 +23,10 @@ resource "aws_elb" "enve-webapp-elb" {
     target              = "HTTP:8080/"
   }
 
-  internal              = true
-  instances             = [aws_instance.enve-webapp-srvr1.id]
+  # instances             = [aws_instance.enve-webapp-srvr1.id]
   idle_timeout          = 400
 
-  tags {
+  tags = {
     Name = "enve-webapp-elb"
   }
 
