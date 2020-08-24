89dd73993535edf03df111508c57cc1d07721956
buglione <mariano@enve.io>
2020-08-23 23:48:20 -0300

    added vpc_zone_identifier param for multiple az deployment

--- asg.tf
+++ asg.tf
@@ -1,17 +1,17 @@
 resource "aws_autoscaling_group" "enve-webapp-asg" {
   launch_configuration = aws_launch_configuration.enve-webapp-lc.name
-  availability_zones = ["us-east-1a","us-east-1c"]
-  # todo
-  # availability_zones = [data.aws_availability_zones.all.names]
-  load_balancers= [aws_elb.enve-webapp-elb.id]
-  health_check_type="ELB"
+  vpc_zone_identifier  = [aws_subnet.enve-labs-pub-sn-a.id, aws_subnet.enve-labs-pub-sn-b.id]
 
   min_size = 2
-  max_size = 5
+  max_size = 6
+
+  load_balancers= [aws_elb.enve-webapp-elb.id]
+  health_check_type="ELB"
 
   tag {
     key                 = "Name"
     value               = "enve-webapp-srvr"
+    terraformed = "true"
     propagate_at_launch = true
   }
 }
