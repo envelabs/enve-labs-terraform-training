75ba4f4af5dd05dd80561c996ccfe0557c44c054
buglione <mariano@enve.io>
2020-08-24 00:06:32 -0300

    added subnets param for multiple az deployment

--- elb.tf
+++ elb.tf
@@ -1,18 +1,14 @@
 resource "aws_elb" "enve-webapp-elb" {
   name               = "enve-webapp-elb"
-  # todo
-  # subnets            = [data.aws_subnet_ids.all.ids]
-  # subnets            = ["subnet-06f02c71", "subnet-3707191f"]
-  # todo
-  # availability_zones = [data.aws_availability_zones.all.names]
-  availability_zones = ["us-east-1a","us-east-1c"]
   security_groups    = [aws_security_group.enve-webapp-elb-sg.id]
+  subnets            = [aws_subnet.enve-labs-pub-sn-a.id,aws_subnet.enve-labs-pub-sn-b.id]
+
 
   listener {
-    instance_port      = var.http_port
-    instance_protocol  = "http"
-    lb_port            = 8080
+    lb_port            = var.http_port
     lb_protocol        = "http"
+    instance_port      = var.webapp_port
+    instance_protocol  = "http"
   }
 
   health_check {
