750368dc16abbc757a5aa81e0a3c7fb5d78f6005
buglione <mariano@enve.io>
2020-08-23 23:44:03 -0300

    minor fixes for variable definitions

--- main.tf
+++ main.tf
@@ -1,15 +1,22 @@
 provider "aws" {
-  region  = "us-east-1"
+  region  = var.region
 }
+
+data "aws_availability_zones" "all" {}
 
 data "aws_vpc" "default" {
   default = true
-}
-
-data "aws_availability_zones" "all" {
 }
 
 data "aws_subnet_ids" "all" {
   vpc_id = data.aws_vpc.default.id
 }
 
+# bucket for terraform state
+terraform {
+  backend "s3" {
+    region = var.region
+    bucket = var.enve-labs-tf-state
+    key    = var.enve-labs-tf-state-key
+  }
+}
--- vars.tf
+++ vars.tf
@@ -2,10 +2,34 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "enve-labs-tf-state" {
+  default = "enve-labs-tf-state"
+}
+
+variable "enve-labs-tf-state-key" {
+  default = "enve-labs-dev-tf-state"
+}
+
+variable "enve-labs-vpc-cidr" {
+  default = "10.1.0.0/16"
+}
+
+variable "subnet-cidr-pub-a" {
+  default = "10.1.1.0/24"
+}
+
+variable "subnet-cidr-pub-b" {
+  default = "10.1.2.0/24"
+}
+
 variable "ssh_port" {
   default = "22"
 }
 
 variable "http_port" {
+  default = "80"
+}
+
+variable "webapp_port" {
   default = "8080"
 }
