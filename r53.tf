68d7f125362b439a51272d65c5483befa3aaa731
buglione <mariano@enve.io>
2020-08-24 00:10:28 -0300

    added support for dns resolutions against the lb

--- r53.tf
+++ r53.tf
@@ -1,9 +1,7 @@
 resource "aws_route53_record" "enve-webapp-srvr1" {
   zone_id = "Z04164709DVPTWJDONB9"
-  name    = "test.envelabs.com"
-  #type    = "A"
+  name    = "webapp.envelabs.com"
   type    = "CNAME"
   ttl     = "300"
-  # records = [aws_instance.enve-webapp-srvr1.public_ip]
   records = [aws_elb.enve-webapp-elb.dns_name]
 }
