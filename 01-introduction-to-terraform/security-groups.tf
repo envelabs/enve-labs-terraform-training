629c897c7667cd66b4932d3189cdc995ebd2a2bf
buglione <mariano@enve.io>
2021-05-15 12:09:27 -0300

    added new sg for backend

--- 01-introduction-to-terraform/security-groups.tf
+++ 01-introduction-to-terraform/security-groups.tf
@@ -40,3 +40,36 @@ resource "aws_security_group" "enve-webapp-sg" {
 
 }
 
+resource "aws_security_group" "enve-backend-sg" {
+  name        = "enve-backend-sg"
+  description = "enve-backend-sg"
+
+  # access for ssh
+  ingress {
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # access for webapp
+  ingress {
+    from_port   = var.webapp_port
+    to_port     = var.webapp_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "enve-backend-sg"
+  }
+
+}
+
