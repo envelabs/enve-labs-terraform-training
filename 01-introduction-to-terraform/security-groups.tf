2077bab707f27939a71ae9f8acb02fffcb662504
buglione <mariano@enve.io>
2021-05-15 10:07:51 -0300

    added http port availability

--- 01-introduction-to-terraform/security-groups.tf
+++ 01-introduction-to-terraform/security-groups.tf
@@ -10,6 +10,14 @@ resource "aws_security_group" "enve-webapp-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # access for http
+  ingress {
+    from_port   = var.http_port
+    to_port     = var.http_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # access for webapp
   ingress {
     from_port   = var.webapp_port
@@ -27,6 +35,7 @@ resource "aws_security_group" "enve-webapp-sg" {
 
   tags = {
     Name = "enve-webapp-sg"
+    env = "dev"
   }
 
 }
